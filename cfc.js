d00cb12bc8111d0aac434be7b25cbfed28674660
JmeMcQ <58938273+JmeMcQ@users.noreply.github.com>
2019-12-19 17:45:33 +0000

    Publish emit newsfeed
    Cheers Lz

--- cfc.js
+++ cfc.js
@@ -1,36 +1,97 @@
 class EventEmitter{
     constructor(){
         this.store = {}
+        this.maxEventListeners = 6;
+        this.eventCount = 0;
 
     }
-    subscribe(name, eventListenerFN){
+    addEventListener(name, eventListenerFN){
         var obj = this.store[name]
         if(!this.store[name]) {
             this.store[name] = []
         }
         this.store[name].push(eventListenerFN)
+        this.eventCount++
+        if(this.eventCount > this.maxEventListeners){
+            throw new Error("Too many event listeners", )
+        }
+        
 
     }
-    publish(name, content){
+    emit (name, content){
         if(this.store[name]){
-            this.store[name].forEach ((element) => element(name, content))
+            this.store[name].forEach((element) => element(name, content))
         }
+    }
+    removeEventListener(name, eventListenerFN) {
+        if(this.store[name]){
+            if (!eventListenerFN){
+                this.eventCount = 0
+                this.store[name] = []
+        } else { 
+            this.store[name] = this.store[name].filter(FN => FN  !== eventListenerFN )
+            this.eventCount--
+        } 
+        }
+    }
+    on(...args){
+        this.addEventListener(...args)
+    }
+    off(...args){
+        this.removeEventListener(...args)
+    }
+}
 
+class Twitter extends EventEmitter {
+    constructor(account){
+        super()
+        this.account = account
+    }
+    tweet(...args){
+        this.emit(...args)
+    }
+    block(...args){
+        this.removeEventListener(...args)
     }
 
 }
+
 var footballNews = new EventEmitter()
+var twitterStream = new Twitter('account')
+    
 
-footballNews.subscribe('ChelseaFC', onFootballNews)
-footballNews.subscribe('liverpool', onFootballNews)
-footballNews.subscribe('liverpool', onBlahNews)
-footballNews.subscribe('liverpool', onNews)
+footballNews.on('ChelseaFC', onFootballNews)
+footballNews.on('liverpool', onFootballNews)
+footballNews.on('liverpool', onBlahNews)
+footballNews.on('liverpool', onNews)
+footballNews.on('Oxford', onBlahNews)
+footballNews.on('Oxford', onNews)
+
+setInterval(()=>{twitterStream.tweet ('@Trump', 'MAGA, build the wall')}, 2000)
+setTimeout(()=> twitterStream.block('@Trump'), 1)
+
+
+twitterStream.addEventListener('Obama', onTweet)
+twitterStream.addEventListener('@Trump', onTweet)
+
+function onTweet(Handle, tweet){
+    console.log(Handle.replace('@', ''), ':', tweet.toString())
+}
 
 
 
-footballNews.publish('ChelseaFC', 'won 4-2')
-footballNews.publish('ChelseaFC', 'champs of the world!!!!!')
-footballNews.publish('liverpool', 'threw away the cup!')
+
+twitterStream.tweet('Obama', 'Buffer.allocUnsafeSlow')
+
+
+footballNews.emit ('ChelseaFC', 'won 4-2')
+footballNews.removeEventListener('ChelseaFC', onFootballNews)
+footballNews.emit ('ChelseaFC', 'champs of the world!!!!!')
+footballNews.emit ('liverpool', 'threw away the cup!')
+footballNews.emit ('Oxford', 'something')
+footballNews.removeEventListener('Oxford', onBlahNews)
+footballNews.removeEventListener('liverpool')
+footballNews.emit ('liverpool',"this is a stirng")
 
 
 
